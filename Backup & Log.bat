5a6cd2bca2c01faa410bbe2066620411515f17f0
Iterativehail27 <53462356+Iterativehail27@users.noreply.github.com>
2023-09-18 17:37:57 -0400

    Update Backup & Log.bat

--- Backup & Log.bat
+++ Backup & Log.bat
@@ -1,1 +1,1 @@
-robocopy C:\Users\__USER__\Records D:\Backup /copy:DATOUS /z /b /MIR /R:5 /W:2 /LOG:C:\logs\backup.log
+robocopy C:\Users\__USER__\Records D:\Backup /copy:DATOUS /fft /zb /MIR /R:5 /W:2 /LOG:C:\logs\backup.log
