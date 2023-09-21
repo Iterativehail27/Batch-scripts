d98ed2ebff40a21da077f516ccc1524ed1fd4aa4
Iterativehail27 <53462356+Iterativehail27@users.noreply.github.com>
2023-09-21 10:05:09 -0400

    Update diskpart_fat_16.bat

--- Diskpart_fat_16/diskpart_fat_16.bat
+++ Diskpart_fat_16/diskpart_fat_16.bat
@@ -14,20 +14,20 @@ if %ERRORLEVEL% NEQ 0 cls && goto D
 :F
 (echo "You are going to format disk %input% to fat 16 4GB size if you've made a mistake press 'n' now" [y:n])
 set /p choice1= 
-if %choice1% == Y goto A
-if %choice1% == y goto A
-if %choice1% == N cls && goto C
-if %choice1% == n cls && goto C 
-if NOT %choice1% == y cls && goto D
+if "%choice1%" == "Y" goto A
+if "%choice1%" == "y" goto A
+if "%choice1%" == "N" cls && goto C
+if "%choice1%" == "n" cls && goto C 
+if NOT "%choice1%" == "y" cls && goto D
 	
 :A
 (echo "Are you sure? This will erase all data on disk %input%. This is your last chance to go make a backup!" [y:n])
 set /p choice2=
-if %choice2% == Y goto B
-if %choice2% == y goto B
-if %choice2% == N cls && goto C
-if %choice2% == n cls && goto C
-if NOT %choice2% == y cls && goto D
+if "%choice2%" == "Y" goto B
+if "%choice2%" == "y" goto B
+if "%choice2%" == "N" cls && goto C
+if "%choice2%" == "n" cls && goto C
+if NOT "%choice2%" == "y" cls && goto D
 
 :B
 set countdown=5
@@ -42,7 +42,7 @@ timeout /t 1 /nobreak >nul
 set /a countdown=%countdown% -1
 if %countdown% NEQ 0 goto :timer_loop
 
-cls
+
 (echo select disk %input% 
 echo clean 
 echo create part primary size=4000
@@ -51,9 +51,10 @@ echo format fs=fat quick) | diskpart
 (echo.)
 (echo Press any key to end)
 pause >nul
-cls
+
 
 :C
+cls
 (echo Terminating)
 set countdown=2
 
@@ -67,17 +68,17 @@ timeout /t 1 /nobreak >nul
 set /a countdown=%countdown% -1
 if %countdown% NEQ 0 goto :timer_loop_2
 
+
 exit
-exit
+exit 
 
 
 :D
 (echo "That was an invalid input!")
 (echo "Do you want to continue?" [y:n])
 set /p choice3=
-if %choice3% == Y  goto Z
-if %choice3% == y  goto Z
-if %choice3% == N cls && goto C
-if %choice3% == n cls && goto C
-if NOT %choice3% == y cls && goto D
- 
+if "%choice3%" == "Y"  goto Z
+if "%choice3%" == "y"  goto Z
+if "%choice3%" == "N" cls && goto C
+if "%choice3%" == "n" cls && goto C
+if NOT "%choice3%" == "y" cls && goto D 
