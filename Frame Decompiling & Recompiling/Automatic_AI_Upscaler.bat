19b2482a8f76f4a7db4a26e817f8ee308469b112
Iterativehail27 <53462356+Iterativehail27@users.noreply.github.com>
2024-08-28 14:22:49 -0400

    Update Automatic_AI_Upscaler.bat

--- Frame Decompiling & Recompiling/Automatic_AI_Upscaler.bat
+++ Frame Decompiling & Recompiling/Automatic_AI_Upscaler.bat
@@ -7,6 +7,10 @@ cd /d ###YOUR DRIVE LETTER HERE###
 cd ###YOUR VIDEO FRAME INPUT DIRECTORY HERE###
 del /s /q *.*
 
+cd /d ###YOUR DRIVE LETTER HERE###
+cd ###YOUR VIDEO STAGING DIRECTORY HERE###
+del /s /q *.*
+
 cd ###YOUR VIDEO STORAGE FOLDER HERE###
 
 FOR %%F IN ("%###YOUR VIDEO STORAGE FOLDER HERE###%\*.mkv") DO (
@@ -18,12 +22,19 @@ echo "%filename%"
 
 move "%filename%" "###YOUR VIDEO FRAME INPUT DIRECTORY HERE###"
 
+cd "###YOUR VIDEO FRAME INPUT DIRECTORY HERE###"
+for %%i in (*.mkv) do (
+    echo Processing "%%i"
+    ffmpeg -i "%%i" "###YOUR VIDEO FRAME INPUT DIRECTORY HERE###\frame%%07d.png"
+    move "###YOUR VIDEO FRAME INPUT DIRECTORY HERE###\frame%%07d.png" "###YOUR VIDEO STAGING FOLDER HERE###"
+)
+
 
 cd ###YOUR AI MODEL FOLDER EXE HERE### 
 
 ### <EXAMPLE> ###
 
-cd "G:\realesrgan-ncnn-vulkan-20220424-windows\realesrgan-ncnn-vulkan.exe" -i "###YOUR VIDEO FRAME INPUT DIRECTORY HERE###" -o "###YOUR VIDEO FRAME INPUT DIRECTORY HERE###" -s 1 -n 4x-UltraSharp-fp32 -f png
+cd "G:\realesrgan-ncnn-vulkan-20220424-windows\realesrgan-ncnn-vulkan.exe" -i "###YOUR VIDEO STAGING FOLDER HERE###" -o "###YOUR VIDEO FRAME INPUT DIRECTORY HERE###" -s 1 -n 4x-UltraSharp-fp32 -f png
 
 ### </EXAMPLE> ###
 
@@ -38,7 +49,6 @@ cd "###YOUR VIDEO FRAME INPUT DIRECTORY HERE###"
 
 for %%i in (*.mkv) do (
     echo Processing "%%i"
-    ffmpeg -i "%%i" "###YOUR VIDEO FRAME INPUT DIRECTORY HERE###\frame%%07d.png"
     ffmpeg -r 23.976023 -i frame%%07d.png -c:v libsvtav1 -crf 1 -pix_fmt yuv420p "###YOUR VIDEO OUTPUT DIRECTORY HERE###\%%i"
 )
 
